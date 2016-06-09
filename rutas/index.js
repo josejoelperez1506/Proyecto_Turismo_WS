9dd81285e046bf0e0d90d8553a13299ff33c2ff3
josejoelperez1506 <joseperezsontay@hotmail.com>
2016-06-09 10:26:39 -0600

    Index

--- rutas/index.js
+++ rutas/index.js
@@ -25,15 +25,41 @@ module.exports=(function(modelo){
     ruta.use(usuario.tokenMiddleware);
 
     ruta.get('/prueba',usuario.prueba)
-    /*
-     Rutas para Contacto
-     */
-    ruta.get('/contacto/:id',contacto.list);
-    ruta.post('/contacto',contacto.add);
-    ruta.put('/contacto/:id',contacto.edit);
-    ruta.delete('/contacto/:id',contacto.delete);
 
+    //Rutas para Departamento
 
+    ruta.get('/departamento/:id',departamento.list);
+    ruta.post('/departamento',departamento.add);
+    ruta.put('/departamento/:id',departamento.edit);
+    ruta.delete('/departamento/:id',departamento.delete);
+
+    //Rutas para Hotel
+
+    ruta.get('/hotel/:id',hotel.list);
+    ruta.post('/hotel',hotel.add);
+    ruta.put('/hotel/:id',hotel.edit);
+    ruta.delete('/hotel/:id',hotel.delete);
+
+    //Rutas para LugarTuristico
+
+    ruta.get('/lugarTuristico/:id',lugarTuristico.list);
+    ruta.post('/lugarTuristico',lugarTuristico.add);
+    ruta.put('/lugarTuristico/:id',lugarTuristico.edit);
+    ruta.delete('/lugarTuristico/:id',lugarTuristico.delete);
+
+    //Rutas para Restaurante
+
+    ruta.get('/restaurante/:id',restaurante.list);
+    ruta.post('/restaurante',restaurante.add);
+    ruta.put('/restaurante/:id',restaurante.edit);
+    ruta.delete('/restaurante/:id',restaurante.delete);
+
+    //Rutas para Usuario
+
+    ruta.get('/usuario/:id',usuario.list);
+    ruta.post('/usuario',usuario.add);
+    ruta.put('/usuario/:id',usuario.edit);
+    ruta.delete('/usuario/:id',usuario.delete);
 
     return ruta;
 });
